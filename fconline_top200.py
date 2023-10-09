25f65ae7d4ca1a2df97a8f23c41139223f6e6160
ggongch2 <111619666+ggongch2@users.noreply.github.com>
2023-10-10 03:43:48 +0900

    Add files via upload

---
+++ fconline_top200.py
@@ -0,0 +1,145 @@
+import selenium
+import time
+import random 
+
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from bs4 import BeautifulSoup 
+
+
+
+
+options = webdriver.ChromeOptions()
+#options.add_argument('--headless')        # Head-less 설정
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(options=options)
+
+
+# 상위 200명 
+
+url = "https://fconline.nexon.com/datacenter/playerstat"
+driver.get(url)
+time.sleep(3)
+
+
+
+descending_button = driver.find_element(By.XPATH, '//*[@id="middle"]/div/div/div[3]/div/div[2]/div[1]/div/div[2]/div/a[1]')
+descending_button.click()
+time.sleep(5)
+
+#데이터들
+season_list = []
+name_list = []
+overall_list = []
+price_list = []
+id_list = []
+
+#전체 리스트
+player_list = []
+
+
+
+
+
+#이미지 추출
+image_class = driver.find_elements(By.CSS_SELECTOR, '.info_top .season') 
+for each in image_class :
+    image_element = each.find_element(By.TAG_NAME, 'img')
+    image_src = image_element.get_attribute('src')
+    image_src_list = image_src.split('/')
+    class_name = image_src_list[-1].split('.')[0]
+    season_list.append(class_name) 
+#시즌 명단 출력 
+for each in season_list :
+    print(each)
+#이름 추출
+name_elements = driver.find_elements(By.CSS_SELECTOR, '.info_top .name')
+for each in name_elements :
+    name_list.append(each.text)
+# 이름 명단 출력 
+for each in name_list :
+    print(each)
+
+
+
+# player_id 를 수집하기 bs4 로 하기 
+
+soup = BeautifulSoup()
+
+
+
+
+
+for info_element in info_elements :
+    class_name_list = info_element.get_attribute('class') 
+    
+    pid = (class_name_list[-1].split('_'))[-1] 
+    print(pid) 
+    
+
+
+
+
+
+
+
+"""
+
+
+
+link  = player_detail.get_attribute('onclick')
+driver.execute_script('window.open(" ");')
+time.sleep(1) 
+driver.switch_to.window(driver.window_handles[1]) 
+driver.get(link) 
+time.sleep(1.5) 
+# 이동후 
+# 오버롤
+overall_element = driver.find_element(By.CLASS_NAME, 'ovr value')
+overall_value = overall_element.text 
+overall_list.append(overall_value) 
+# 가격
+price_element = driver.find_element(By.CSS_SELECTOR, '.add_info .txt')
+price_text = price_element.text 
+price_value = price_text.split('\n')[1].strip()
+price_list.append(price_value) 
+#탭 닫기 및 원래대로 이동 
+driver.close() 
+driver.switch_to.window(driver.window_handles[0])
+time.sleep(1.4+random.random())
+      
+"""
+      
+
+
+
+#print('season_list :', len(season_list), 'name_list :', len(name_list))
+
+#하나로 묶기
+for i in range(0, len(season_list)) :
+    name = name_list[i]
+    season = season_list[i] 
+    overall = overall_list[i]
+    price = price_list[i] 
+
+    p_list = [name, season, overall, price]
+    player_list.append(p_list) 
+
+
+print(player_list)
+
+#텍스트 파일로 저장
+with open('top200_player_list.txt', 'w', encoding='UTF-8') as f :
+    for name in player_list :
+        f.write(name[0]+" "+name[1]+"\n") 
+
+#<a href="#" onclick="DataCenter.GetPlayerDetail(this,287193301,'',2)" class="btn_detail_link"></a>
+
+
+
+
+
